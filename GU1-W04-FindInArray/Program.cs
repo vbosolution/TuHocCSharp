30859672a574e924f10c25f4c6b2e70594d2430c
vbosolution <125480004+vbosolution@users.noreply.github.com>
2024-01-27 22:35:24 +0700

    Learn Array.IndexOf

--- GU1-W04-FindInArray/Program.cs
+++ GU1-W04-FindInArray/Program.cs
@@ -24,19 +24,27 @@ namespace GU1_W04_FindInArray
             };
             Console.Write("Enter a name’s student: ");
             string input_name = Console.ReadLine();
-            bool isExist = false;
-            for (int i = 0; i < students.Length; i++)
-            {
-                if (students[i] == input_name)
-                {
-                    Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
-                    isExist = true;
-                    break;
-                }
-            }
-            if (!isExist)
+            //bool isExist = false;
+            //for (int i = 0; i < students.Length; i++)
+            //{
+            //    if (students[i] == input_name)
+            //    {
+            //        Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+            //        isExist = true;
+            //        break;
+            //    }
+            //}
+            //if (!isExist)
+            //{
+            //    Console.WriteLine("Not found" + input_name + " in the list.");
+            //}
+            int index = Array.IndexOf(students, input_name);
+            if (index == -1)
             {
                 Console.WriteLine("Not found" + input_name + " in the list.");
+            } else
+            {
+                Console.WriteLine("Position of the students in the list " + input_name + " is: " + index);
             }
             Console.Write("Press any key...");
             Console.ReadKey();
