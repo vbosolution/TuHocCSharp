4b87fd04a127450507c9347cf8b632e6e96e5234
vbosolution <125480004+vbosolution@users.noreply.github.com>
2024-02-22 19:46:57 +0700

    Add Mang nhieu chieu

---
+++ 240219 - MangNhieuChieu/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _240219___MangNhieuChieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = {
+                { 1, 2, 3, 4, 5, 6, 7 },
+                { 1, 12, 3, 4, 5, 6, 7},
+                { 1, 2, 33, 4, 5, 96, 7},
+                { 1, 24, 3, 4, 5, 6, 7},
+                { 1, 2, 3, 4, 15, 6, 7}
+            };
+            Console.WriteLine("Matrix:");
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0;
+            int max = a[0, 0];
+            int ii = 0, jj = 0;
+            for (int i=0; i<a.GetLength(0); i++)
+            {
+                for (int j=0; j<a.GetLength(1); j++)
+                {
+                    if (max < a[i, j])
+                    {
+                        max = a[i, j];
+                        ii = i;
+                        jj = j;
+                    }
+                    if (i == j)
+                    {
+                        sum += a[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Sum all elements on the main diagonal : " + sum);
+            Console.WriteLine("Max elements: " + max + " found at: (" + ii + "," + jj + ")");
+            Console.ReadKey();
+        }
+    }
+}
--- GU1-W04-InsertAValueAtIndex/Program.cs
+++ GU1-W04-InsertAValueAtIndex/Program.cs
@@ -10,6 +10,33 @@ namespace GU1_W04_InsertAValueAtIndex
     {
         static void Main(string[] args)
         {
+            int[] ar = { 10, 4, 6, 7, 8, 0, 0, 0, 0, 0 };
+            Console.WriteLine("Array");
+            foreach (int i in ar)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Input the insert number: ");
+            int insert = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Input the index of insertion: ");
+            int index = Int32.Parse(Console.ReadLine());
+
+            for (int i = ar.Length - 1; i > index; i--)
+            {
+                ar[i] = ar[i - 1];
+            }
+            ar[index] = insert;
+
+            Console.WriteLine("Array after the insertion");
+            foreach (int i in ar)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
