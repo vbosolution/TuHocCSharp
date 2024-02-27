b4056868ebfa3a841c7cbb872996575fc13f5871
vbosolution <125480004+vbosolution@users.noreply.github.com>
2024-02-27 20:39:00 +0700

    added rectangle

---
+++ 240227 - OOP - Rectangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__240227_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rectangle project.");
+            Console.Write("Input rectangle's width: ");
+            double w = Double.Parse(Console.ReadLine());
+            Console.Write("Input rectangle's height: ");
+            double h = Double.Parse(Console.ReadLine());
+            Rectangle rect = new Rectangle(w, h);
+            Console.WriteLine(rect.Inspect());
+            Console.WriteLine($"Area: {rect.Area()}");
+            Console.WriteLine($"Perimeter: {rect.Perimeter()}");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ 240227 - OOP - Rectangle/Rect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Rectangle
+{
+    double width, height;
+    public Rectangle()
+    {
+        this.width = 10.0;
+        this.width = 5.0;
+    }
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public double Area()
+    {
+        return this.width * this.height;
+    }
+    public double Perimeter()
+    {
+        return 2 * (this.width + this.height);
+    }
+    public string Inspect()
+    {
+        return $"Rectangle (WxH) {this.width} x {this.height}";
+    }
+}
