5a427fe923c7c4c3976742aae42c867f0e18b704
vbosolution <125480004+vbosolution@users.noreply.github.com>
2024-02-28 16:14:35 +0700

    static method

---
+++ GU1-W06-Static Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W06_Static_Method
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Student.Change();
+
+            Student s1 = new Student(111, "Hoang");
+            Student s2 = new Student(222, "Khanh");
+            Student s3 = new Student(333, "Nam");
+
+            s1.Display();
+            s2.Display();
+            s3.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GU1-W06-Static Method/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W06_Static_Method
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+
+
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+
+        // Likes class methods vs instance methods in Ruby
+        // static method is a class method, called by class#method
+
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+
+        // this is an instance method. called by the object#method
+
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
