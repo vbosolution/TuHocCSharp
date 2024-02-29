e1775c014e1de23b1d3e8aacf72c8ef0f39e96e1
vbosolution <125480004+vbosolution@users.noreply.github.com>
2024-02-29 19:43:27 +0700

    f2c

---
+++ GU1-W05-F2C/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W05_F2C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
