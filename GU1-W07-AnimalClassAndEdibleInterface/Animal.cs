7272a4ba7539eebc98f7dee54460c839cc675d67
Lê Việt Trường <leviettruong@vbosolution.com>
2024-03-16 17:01:20 +0700

    Interface

---
+++ GU1-W07-AnimalClassAndEdibleInterface/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W07_AnimalClassAndEdibleInterface
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+    public class Tiger : Animal
+    {
+        public override string MakeSound()
+        {
+            return "Tiger: roarrrrr!";
+        }
+    }
+    public class Chicken : Animal, IEdible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+
+    public interface IEdible
+    {
+        string HowToEat();
+    }
+}
---
+++ GU1-W07-AnimalClassAndEdibleInterface/Fruit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W07_AnimalClassAndEdibleInterface
+{
+    public abstract class Fruit : IEdible
+    {
+        public abstract string HowToEat();
+    }
+    public class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+    public class Orange : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+}
