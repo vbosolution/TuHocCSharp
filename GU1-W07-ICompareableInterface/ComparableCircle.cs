e2f59ea0d36d59cccf7523b39875dc8ba77dbf2d
Lê Việt Trường <leviettruong@vbosolution.com>
2024-03-16 17:41:34 +0700

    IComparable Interface

---
+++ GU1-W07-ICompareableInterface/ComparableCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W07_ICompareableInterface
+{
+    public class ComperableCircle : Circle, IComparable<ComperableCircle>
+    {
+
+        public ComperableCircle() { }
+        public ComperableCircle(Circle c) : base(c.getRadius()) { }
+        public ComperableCircle(double radius) : base(radius) { }
+        public ComperableCircle(double radius, string color, bool filled) : base(radius, color, filled) { }
+
+        public int CompareTo(ComperableCircle o)
+        {
+            if (getRadius() > o.getRadius()) return 1;
+            else if (getRadius() < o.getRadius()) return -1;
+            else return 0;
+        }
+    }
+}
---
+++ GU1-W07-ICompareableInterface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W07_ICompareableInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5.0);
+            ComperableCircle[] circles = new ComperableCircle[4];
+            circles[0] = new ComperableCircle(3.6);
+            circles[1] = new ComperableCircle();
+            circles[2] = new ComperableCircle(3.5, "indigo", false);
+            circles[3] = new ComperableCircle(c);
+
+            Console.WriteLine("Pre-sorted:");
+            foreach (ComperableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+
+            Array.Sort(circles);
+
+            Console.WriteLine("After-sorted:");
+            foreach (ComperableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+            Console.ReadKey();
+        }
+    }
+}
